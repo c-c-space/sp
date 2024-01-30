a4db29aef5743300388f25bf170691676cf4918c
pe-hu <we.are.pe.hu@gmail.com>
2024-01-30 15:30:31 +0900

    Update script.js

--- sign/script.js
+++ sign/script.js
@@ -1,40 +1,3 @@
-let signArr
-
-if (hour >= 11 && hour <= 15) {
-    signArr = [
-        "sign/day/001.gif",
-        "sign/day/002.gif",
-        "sign/day/003.gif",
-        "sign/day/004.gif",
-        "sign/day/005.gif"
-    ]
-} else if (hour >= 16 && hour <= 20) {
-    signArr = [
-        "sign/night/001.gif",
-        "sign/night/002.gif",
-        "sign/night/003.gif",
-        "sign/night/004.gif"
-    ]
-} else {
-    signArr = [
-        "sign/close/001.jpeg",
-        "sign/close/002.jpeg",
-        "sign/close/003.jpeg",
-        "sign/close/004.jpeg",
-        "sign/close/005.jpeg",
-        "sign/close/006.jpeg"
-    ]
-}
-
-const signAll = [
-    "sign/day/001.gif",
-    "sign/day/002.gif",
-    "sign/night/001.gif",
-    "sign/night/004.gif",
-    "sign/day/004.gif",
-    "sign/day/005.gif"
-]
-
 function changeHidden() {
     const mainAll = document.querySelectorAll('main')
     mainAll.forEach(main => {
@@ -68,7 +31,7 @@ async function sign(gradient, flash, csv) {
     cover.style.backgroundImage = `linear-gradient(0deg, ${gradientAll} #fff)`;
 
     const qr = document.createElement('img')
-    qr.src = 'qr.png'
+    qr.src = 'https://creative-community.space/sp/qr.png'
     qr.art = 'https://creative-community.space/sp/'
 
     window.addEventListener("beforeprint", () => {
