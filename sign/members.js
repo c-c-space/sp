9faad4474594d9f0f8aae5a166db200ff7aae191
pe-hu <we.are.pe.hu@gmail.com>
2024-01-30 15:25:00 +0900

    Update members.js

--- sign/members.js
+++ sign/members.js
@@ -29,4 +29,19 @@ document.addEventListener('readystatechange', event => {
       window.location.assign(selectMonth[index].value)
     })
   }
+
+  const scrollElementAll = document.querySelectorAll('main');
+  scrollElementAll.forEach((scrollElement) => {
+    scrollElement.addEventListener('wheel', (e) => {
+      if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+      const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
+      if (
+        (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
+        (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
+      )
+        return;
+      e.preventDefault();
+      scrollElement.scrollLeft += e.deltaY;
+    });
+  });
 })
